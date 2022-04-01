e6a111743a987be00dce64b5b2def17d0b64ec32
katiavas <bl19548@bristol.ac.uk>
2022-04-01 21:08:11 +0100

    commit

--- curiosity-Cartpole-simple/memory.py
+++ curiosity-Cartpole-simple/memory.py
@@ -10,10 +10,10 @@ class Memory:
 
     def __init__(self):
         # T.use_deterministic_algorithms(True)
-        self.seed = 111
-        random.seed(self.seed)
-        np.random.seed(self.seed)
-        T.manual_seed(self.seed)
+        # self.seed = 111
+        # random.seed(self.seed)
+        # np.random.seed(self.seed)
+        # T.manual_seed(self.seed)
         self.states = []
         self.actions = []
         self.rewards = []
@@ -23,9 +23,9 @@ class Memory:
 
     def remember(self, state, action, reward, new_state, value, log_p):
         # T.use_deterministic_algorithms(True)
-        random.seed(self.seed)
-        np.random.seed(self.seed)
-        T.manual_seed(self.seed)
+        # random.seed(self.seed)
+        # np.random.seed(self.seed)
+        # T.manual_seed(self.seed)
         self.actions.append(action)
         self.rewards.append(reward)
         self.states.append(state)
@@ -34,9 +34,9 @@ class Memory:
         self.values.append(value)
 
     def clear_memory(self):
-        random.seed(self.seed)
-        np.random.seed(self.seed)
-        T.manual_seed(self.seed)
+        # random.seed(self.seed)
+        # np.random.seed(self.seed)
+        # T.manual_seed(self.seed)
         self.states = []
         self.actions = []
         self.rewards = []
@@ -45,9 +45,9 @@ class Memory:
         self.log_probs = []
 
     def sample_memory(self):
-        random.seed(self.seed)
-        np.random.seed(self.seed)
-        T.manual_seed(self.seed)
+        # random.seed(self.seed)
+        # np.random.seed(self.seed)
+        # T.manual_seed(self.seed)
 
         return self.states, self.actions, self.rewards, self.new_states,\
                self.values, self.log_probs
