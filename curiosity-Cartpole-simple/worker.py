f26c893fb32c05bfed0f8915f87d51eda68fb016
katiavas <bl19548@bristol.ac.uk>
2022-04-01 21:15:45 +0100

    commit

--- curiosity-Cartpole-simple/worker.py
+++ curiosity-Cartpole-simple/worker.py
@@ -43,8 +43,8 @@ def worker(name, input_shape, n_actions, global_agent, global_icm,
         # env.seed(SEED)
         # env.action_space.seed(SEED)
         obs = env.reset()
-        # env.seed(SEED)
-        env.action_space.seed(SEED)
+        env.seed(SEED)
+        # env.action_space.seed(SEED)
         # make your hidden state for the actor critic a3c
         hx = T.zeros(1, 256)
         # we need a score, a terminal flag and the number of steps taken withing the episode
