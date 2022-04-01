925d395747738e976bb424940abbadbfd53e0bd0
katiavas <bl19548@bristol.ac.uk>
2022-04-01 21:09:36 +0100

    commit

--- curiosity-Cartpole-simple/memory.py
+++ curiosity-Cartpole-simple/memory.py
@@ -10,10 +10,10 @@ class Memory:
 
     def __init__(self):
         # T.use_deterministic_algorithms(True)
-        # self.seed = 111
-        # random.seed(self.seed)
-        # np.random.seed(self.seed)
-        # T.manual_seed(self.seed)
+        self.seed = 111
+        random.seed(self.seed)
+        np.random.seed(self.seed)
+        T.manual_seed(self.seed)
         self.states = []
         self.actions = []
         self.rewards = []
