c76a71b8eef77f4d0873565ee2d2ba97a80157cf
katiavas <bl19548@bristol.ac.uk>
2022-04-25 11:54:57 +0100

    commit

--- curiosity-Cartpole-simple/worker.py
+++ curiosity-Cartpole-simple/worker.py
@@ -221,7 +221,7 @@ def worker(name, input_shape, n_actions, global_agent,
     # frame_buffer = [input_shape[1], input_shape[2], 1]
     # env = make_atari(env_id, shape=frame_buffer)
 
-    episode, max_steps, t_steps, scores = 0, 15000, 0, []
+    episode, max_steps, t_steps, scores = 0, 20000, 0, []
     intr = []
     l = []
     l_i = []
